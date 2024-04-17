3f1295e989f9f7e2cc8ebd56e0f6df3f4759a193
Eduardo Correia <eduardo2002correia.com>
2024-04-17 20:13:15 +0100

    frontend-update-incomingPaymenst-paymentsHistoric

---
+++ backend/src/main/java/com/example/backend/Payments/PaymentInfo.java
@@ -0,0 +1,13 @@
+package com.example.backend.Payments;
+
+public class PaymentInfo {
+        public String id;
+        public String name;
+        public String email;
+
+    public PaymentInfo(String id, String name, String email) {
+        this.id = id;
+        this.name = name;
+        this.email = email;
+    }
+}
